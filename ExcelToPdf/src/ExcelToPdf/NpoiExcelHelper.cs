db05aeb353f73a13ae56979c56c51689511297a8
yitian.chen <48295852@qq.com>
2022-01-21 17:52:43 +0800

    初始化项目

---
+++ ExcelToPdf/src/ExcelToPdf/NpoiExcelHelper.cs
@@ -0,0 +1,98 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.Converter;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Text.RegularExpressions;
+
+namespace ExcelToPdf
+{
+    /// <summary>
+    /// 基于Npoi的Excel处理类
+    /// </summary>
+    public class NpoiExcelHelper
+    {
+        /// <summary>
+        /// excel转html
+        /// <paramref name="excelPath">excel文件路径</paramref>
+        /// <paramref name="htmlFilePath">html文件地址</paramref>
+        /// <paramref name="removeSheetName">是否移除生成后的Sheet名称</paramref>
+        /// <paramref name="options">配置转换器</paramref>
+        /// <paramref name="afterProcess">处理完成后的动作</paramref>
+        /// </summary>
+        public static void ExcelToHtml(string excelPath, string htmlFilePath, bool removeSheetName = true, Action<ExcelToHtmlConverter> options = null, Action<ExcelToHtmlConverter> afterProcess = null)
+        {
+            IWorkbook workbook;
+            FileStream fs = new FileStream(excelPath, FileMode.Open, FileAccess.ReadWrite);
+
+            string fileExt = Path.GetExtension(excelPath).ToLower();
+            if (fileExt == ".xlsx")
+            {
+                workbook = new XSSFWorkbook(fs);
+            }
+            else if (fileExt == ".xls")
+            {
+                workbook = new HSSFWorkbook(fs);
+            }
+            else
+            {
+                throw new Exception($"不支持文件格式{fileExt}");
+            }
+
+            ExcelToHtml(workbook, htmlFilePath, removeSheetName, options, afterProcess);
+            workbook.Close();
+            fs.Close();
+        }
+
+        /// <summary>
+        /// excel转html
+        /// <paramref name="workbook">工作簿对象</paramref>
+        /// <paramref name="htmlFilePath">html文件地址</paramref>
+        /// <paramref name="removeSheetName">是否移除生成后的Sheet名称</paramref>
+        /// <paramref name="options">配置转换器</paramref>
+        /// <paramref name="afterProcess">处理完成后的动作</paramref>
+        /// </summary>
+        public static void ExcelToHtml(IWorkbook workbook, string htmlFilePath, bool removeSheetName = true, Action<ExcelToHtmlConverter> options = null, Action<ExcelToHtmlConverter> afterProcess = null)
+        {
+            ExcelToHtmlConverter excelToHtmlConverter = new ExcelToHtmlConverter();
+
+            // 配置转换器
+            if (options != null)
+            {
+                options(excelToHtmlConverter);
+            }
+            else
+            {
+                // 设置输出参数			
+                excelToHtmlConverter.OutputColumnHeaders = false;
+                excelToHtmlConverter.OutputRowNumbers = false;
+            }
+
+            // 处理
+            excelToHtmlConverter.ProcessWorkbook(workbook);
+
+            // 如果要移除SheetName
+            if (removeSheetName)
+            {
+                // 去掉html中的sheet名称
+                var clearHtml = Regex.Replace(excelToHtmlConverter.Document.InnerXml, "<h2>.*</h2>", "");
+                excelToHtmlConverter.Document.InnerXml = clearHtml;
+            }
+
+            // 处理后工作
+            if (afterProcess != null)
+            {
+                afterProcess(excelToHtmlConverter);
+            }
+
+            //添加表格样式 修复linux环境下转pdf后换行的问题
+            excelToHtmlConverter.Document.InnerXml =
+                excelToHtmlConverter.Document.InnerXml.Insert(
+                    excelToHtmlConverter.Document.InnerXml.IndexOf("</style>", 0) - 1,
+                    @" table, tr, td, th, tbody, thead, tfoot { page-break-inside: avoid!important; }"
+                );
+
+            // 输出
+            excelToHtmlConverter.Document.Save(htmlFilePath);
+        }
+    }
+}
---
+++ ExcelToPdf/src/ExcelToPdf/PdfHelper.cs
@@ -0,0 +1,57 @@
+﻿using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.IO;
+using SixLabors.Fonts;
+using System;
+using System.Globalization;
+using System.IO;
+using WkHtmlToPdfDotNet;
+using WkHtmlToPdfDotNet.Contracts;
+
+namespace ExcelToPdf
+{
+    /// <summary>
+    /// Pdf帮助器
+    /// </summary>
+    public static class PdfHelper
+    {
+        /// <summary>
+        /// Html转Pdf
+        /// </summary>
+        /// <param name="htmlFilePath"></param>
+        /// <param name="pdfFilePath"></param>
+        /// <param name="converter">转换器</param>
+        /// <param name="docFactory"></param>
+        public static void HtmlToPdf(this IConverter converter, string htmlFilePath, string pdfFilePath, Func<HtmlToPdfDocument> docFactory = null)
+        {
+            HtmlToPdfDocument doc = null;
+
+            // 如果为配置，那么使用默认设置
+            if (docFactory != null)
+            {
+                doc = docFactory();
+            }
+            else
+            {
+                doc = new HtmlToPdfDocument()
+                {
+                    GlobalSettings = {
+                        ColorMode = ColorMode.Color,
+                        Orientation = Orientation.Portrait,
+                        PaperSize = PaperKind.A4,
+                        Margins = new MarginSettings() { Top = 10 },
+                        Out = pdfFilePath,
+                    },
+                    Objects = {
+                        new ObjectSettings()
+                        {
+                            Page = htmlFilePath,
+                        },
+                    }
+                };
+            }
+
+            converter.Convert(doc);
+        }
+    }
+}
